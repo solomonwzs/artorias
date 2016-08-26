23c140a4c57307c81ebe9fa3fc7736a9a0bc2019
solomon <solomon.wzs@gmail.com>
2016-08-26 19:17:58 +0800

    modified:   .gitignore 	modified:   src/gotp/genserver/behaviour.go 	modified:   src/gotp/keyatom.go

--- src/gotp/genserver/behaviour.go
+++ src/gotp/genserver/behaviour.go
@@ -1,23 +1,69 @@
 package genserver
 
-import "gotp"
+import (
+	"gotp"
+	"time"
+)
 
 type Options struct {
 }
 
 type InitResult struct {
-	Status gotp.KeyAtom
+	Status     gotp.KeyAtom
+	State      interface{}
+	StopReason interface{}
+	Timeout    time.Duration
+}
+
+type CallResult struct {
+	Status     gotp.KeyAtom
+	Replay     interface{}
+	NewState   interface{}
+	StopReason interface{}
+	Timeout    time.Duration
 }
 
 type GenServerModule interface {
-	Init(args []interface{}) (state interface{}, err error)
+	Init(args []interface{}) (result *InitResult)
 	Terminate(reason interface{}, state interface{})
 
 	HandleCall(request interface{}, from *gotp.MessageChannel, state interface{})
 	HandleCast(request interface{}, state interface{})
 }
 
-func Start(module GenServerModule, args []interface{}, options *Options) (
-	pid *gotp.Pid, err error) {
-	return nil, nil
+func OkInitResult(state interface{}) *InitResult {
+	return &InitResult{
+		Status: gotp.ATOM_OK,
+		State:  state,
+	}
+}
+
+func IgnoreInitResult() *InitResult {
+	return &InitResult{
+		Status: gotp.ATOM_IGNORE,
+	}
+}
+
+func StopInitResult(stopReason interface{}) *InitResult {
+	return &InitResult{
+		Status:     gotp.ATOM_STOP,
+		StopReason: stopReason,
+	}
+}
+
+func ReplyCallResult(replay interface{}, newState interface{}) *CallResult {
+	return &CallResult{
+		Status:   gotp.ATOM_REPLY,
+		Replay:   replay,
+		NewState: newState,
+	}
+}
+
+func StopCallResult(stopReason interface{}, replay interface{}, newState interface{}) *CallResult {
+	return &CallResult{
+		Status:     gotp.ATOM_STOP,
+		StopReason: stopReason,
+		Replay:     replay,
+		NewState:   newState,
+	}
 }
--- src/gotp/keyatom.go
+++ src/gotp/keyatom.go
@@ -7,4 +7,6 @@ var (
 	ATOM_STOP      = KeyAtom(0x0001)
 	ATOM_HIBERNATE = KeyAtom(0x0002)
 	ATOM_IGNORE    = KeyAtom(0x0003)
+	ATOM_REPLY     = KeyAtom(0x0004)
+	ATOM_NOREPLY   = KeyAtom(0x0005)
 )
