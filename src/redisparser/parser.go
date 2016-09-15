8a0d768172fc86eea10b481df3ef84da01b52889
solomon <solomon.wzs@gmail.com>
2016-09-16 01:31:26 +0800

    modified:   src/redisparser/parser.go

--- src/redisparser/parser.go
+++ src/redisparser/parser.go
@@ -1,10 +1,10 @@
 package redisparser
 
+import "strconv"
+
 const (
 	_SM_START = iota
 	_SM_UNITL_CRLF
-	_SM_UNITL_CRLF_END
-	_SM_UNITL_CRLF_OK
 	_SM_END
 	_SM_ERROR
 )
@@ -28,12 +28,44 @@ func (rcv *redisCommandValue) setInt(i int) {
 	rcv._Int = i
 }
 
-func (rcv *redisCommandValue) setBytes(b []byte) {
-	rcv._Bytes = b
+func (rcv *redisCommandValue) setBytes(bs []byte) {
+	rcv._Bytes = bs
 }
 
 func (rcv *redisCommandValue) setCommands(c []*RedisCommand) {
 	rcv._Commands = c
+}
+
+func (rcv *redisCommandValue) appendToBuffer(b byte) {
+	rcv._Bytes = append(rcv._Bytes, b)
+}
+
+func (rcv *redisCommandValue) bufferEndByCtrf() bool {
+	l := len(rcv._Bytes)
+	return l >= 2 && rcv._Bytes[l-2] == '\r' && rcv._Bytes[l-1] == '\n'
+}
+
+func (rcv *redisCommandValue) bufferToBytes() error {
+	l := len(rcv._Bytes)
+	rcv._Bytes = rcv._Bytes[:l-2]
+	return nil
+}
+
+func (rcv *redisCommandValue) bufferToInt() error {
+	l := len(rcv._Bytes)
+	i, err := strconv.Atoi(string(rcv._Bytes[:l-2]))
+	if err == nil {
+		rcv._Int = i
+	}
+	return err
+}
+
+func newRedisCommandValue() *redisCommandValue {
+	return &redisCommandValue{
+		_Int:      0,
+		_Bytes:    []byte{},
+		_Commands: nil,
+	}
 }
 
 type RedisCommand struct {
@@ -44,12 +76,8 @@ type RedisCommand struct {
 
 func NewRedisCommand() *RedisCommand {
 	rc := &RedisCommand{
-		_type: RC_TYPE_UNKNOWN,
-		value: &redisCommandValue{
-			_Int:      0,
-			_Bytes:    nil,
-			_Commands: nil,
-		},
+		_type:  RC_TYPE_UNKNOWN,
+		value:  newRedisCommandValue(),
 		status: _SM_START,
 	}
 
@@ -58,7 +86,6 @@ func NewRedisCommand() *RedisCommand {
 
 func (rc *RedisCommand) Parser() {
 	stack := []*RedisCommand{rc}
-	buf := []byte{}
 
 	parser := func(bs []byte) {
 		for _, b := range bs {
@@ -67,6 +94,7 @@ func (rc *RedisCommand) Parser() {
 			case _SM_START:
 				parseStart(b, r)
 			case _SM_UNITL_CRLF:
+				parseUnitlCrlf(b, r)
 			}
 		}
 	}
@@ -91,7 +119,20 @@ func parseStart(b byte, rc *RedisCommand) {
 	}
 }
 
-func parseUnitlCrlf(b byte, rc *RedisCommand, buf *[]byte) {
-	if b == '\n' && len(*buf) > 0 && (*buf)[len(*buf)-1] == '\r' {
+func parseUnitlCrlf(b byte, rc *RedisCommand) {
+	rc.value.appendToBuffer(b)
+	if rc.value.bufferEndByCtrf() {
+		var err error = nil
+		if rc._type == RC_TYPE_STATUS || rc._type == RC_TYPE_ERROR {
+			err = rc.value.bufferToBytes()
+			rc.status = _SM_END
+		} else if rc._type == RC_TYPE_INT {
+			err = rc.value.bufferToInt()
+			rc.status = _SM_END
+		}
+
+		if err != nil {
+			rc.status = _SM_ERROR
+		}
 	}
 }
